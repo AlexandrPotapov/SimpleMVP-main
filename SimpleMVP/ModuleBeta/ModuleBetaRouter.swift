8ac899e1ae6674d41645dff10338f82713f2ac59
alexandr potapov <procrastinator1994@gmail.com>
2024-10-29 13:44:29 +0300

    Initial Commit

---
+++ SimpleMVP/ModuleBeta/ModuleBetaRouter.swift
@@ -0,0 +1,45 @@
+//
+//  ModuleBetaRouter.swift
+//  SimpleMVP
+//
+//  Created by Dmitriy Mirovodin on 18.02.2024.
+
+import UIKit
+
+protocol ModuleBetaRouterProtocol {
+    func showSuccess()
+    func showError()
+}
+
+final class ModuleBetaRouter: ModuleBetaRouterProtocol {
+        
+    weak var root: UIViewController?
+    
+    private let alertFactory: AlertModuleFactory
+    
+    init(alertFactory: AlertModuleFactory) {
+        self.alertFactory = alertFactory
+    }
+    
+    func setRootViewController(root: UIViewController) {
+        self.root = root
+    }
+    
+    func showSuccess() {
+        let viewController = alertFactory.make(
+            title: "Module Beta",
+            message: "Save success"
+        )
+        
+        root?.present(viewController, animated: true)
+    }
+    
+    func showError() {
+        let viewController = alertFactory.make(
+            title: "Module Beta",
+            message: "Save error"
+        )
+        
+        root?.present(viewController, animated: true)
+    }
+}
